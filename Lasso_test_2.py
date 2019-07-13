ee9096e5c17275973366034a0539ffe393b2b6ba
Jesse <jzhou242@optusnet.com.au>
2019-07-13 12:41:29 +0800

    Very close with multidimensional Lasso, we increased the number of iterations, decreased alpha values, ref: Lasso_test_2.py

---
+++ Lasso_test_2.py
@@ -0,0 +1,46 @@
+import pylab, numpy as np
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+
+def init():
+	global x, xarray
+	x = 0.1
+	xarray = [x]
+
+def iterate():
+	global x, xarray
+	x = 4*x*(1-x)
+	xarray.append(x)
+
+def lists(tau):
+	global x, xarray
+	varray = []
+	xtarray = []	
+	i = 2*tau
+	while(i < len(xarray)-1):
+		v = [xarray[i], xarray[i-tau], xarray[i-2*tau]]
+		varray.append(v)
+		xtarray.append(xarray[i+1]+pylab.uniform(-0.05, 0.05))
+		i += 1
+	return varray, xtarray
+
+init()
+print("Initialisation done!")
+for t in range(100000):
+	iterate()
+
+print("Iteration done!")
+a,b = lists(1)
+poly = PolynomialFeatures(degree = 2, include_bias = False)
+c = poly.fit_transform(a)
+print(c)
+print(b)
+
+
+reg = linear_model.Lasso(alpha=0.0001, fit_intercept = True)
+d = reg.fit(c,b)
+y_pred = reg.predict(c)
+print(d.coef_)
+pylab.plot(a[0:],b, "ro")
+pylab.plot(a[0:], y_pred, "b-")
+pylab.show()
